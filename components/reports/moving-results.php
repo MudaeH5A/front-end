8e3175dfb2cebd0d123bb5263dfd4c5b1b764b95
Vinicius Ribeiro <vinicius.fernandes@escale.com.br>
2018-10-21 10:37:33 -0300

    Update prices

--- components/reports/moving-results.php
+++ components/reports/moving-results.php
@@ -1,39 +1,54 @@
 <div class="blog-card animated fadein delay-1">
     <div class="blog-image">
-        <img src="img/10.jpg" alt="">
+        <img src="img/undrawAgreementAajr@3x.png" alt="">
     </div>
 
     <div class="blog-preview p-20">
-        <h4 class="uppercase title-blue">Gustavo encontramos um transportador  para 28/10 às 10:00h</h4>
+        <h3 class="title-blue m-b-20">Encontramos um transportador disponível no dia
+            <span class="avaliable">28/10 às 10h</span>
+        </h3>
+
         <p>
-            <b>Endereço de partida:</b>
-            Luverci Pereira de Souza, 2410
+            <b>Endereço de partida:</b><br>
+            Rua General Júlio Marcondes Salgado
+            Campos Elíseos, São Paulo - SP, CEP 01201-020
         </p>
 
         <p>
-            <b>Endereço final:</b>
-            Av. Francisco Glicério, 829
+            <b>Endereço final:</b><br>
+            Rua Araújo
+            República, São Paulo - SP, CEP 01220-020
         </p>
 
         <p>
-            <b>Percurso:</b>
+            <b>Percurso:</b><br>
             50 quilomêtros
-         </p>
+        </p>
 
-        <p><b>Valor por km: </b>
-            R$ 3,00 Serviço: 100,00
+        <p><b>Valor por km: </b><br>
+            R$ 3,00
+        </p>
+
+        <p>
+            <b>Serviço:</b><br>
+            R$ 100,00
+        </p>
+
+        <p>
+            <b>Valor total:</b><br>
+            <span class="total-value"> R$ 250,00 </span>
         </p>
     </div>
 
     <div class="blog-header">
         <img class="avatar circle" src="img/avatar.png" alt="">
         <div class="blog-author">
-        <span>Igor Paiva</span>
+        <span class="user-name">Vinícius Ribeiro</span>
         <span class="small">Mercedez Bens - Placa GVP 9028</span>
         </div>
     </div>
 
     <div class="p-20">
-        <a class="waves-effect waves-light btn-large primary-color block animated bouncein delay-4 btn-rounded" href="#">Contratar Mudança</a>
+        <a class="waves-effect waves-light btn-large success-color block animated bouncein delay-4 btn-rounded" href="#">Contratar Mudança</a>
     </div>
 </div>
