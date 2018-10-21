906aaea075881a9cf4090f55667a3c55466756f7
Vinicius Ribeiro <vinicius.fernandes@escale.com.br>
2018-10-21 07:30:58 -0300

    Add moving results

--- components/reports/moving-results.php
+++ components/reports/moving-results.php
@@ -5,10 +5,21 @@
 
     <div class="blog-preview p-20">
         <h4 class="uppercase">Gustavo encontramos um transportador  para 28/10 às 10:00h</h4>
-        <p>Endereço de partida: Luverci Pereira de Souza, 2410</p>
-        <p>Endereço final: Av. Francisco Glicério, 829</p>
-        <p>Percurso: 50 quilomêtros</p>
-        <p>Valor por km: R$ 3,00 Serviço: 100,00</p>
+        <p><b>Endereço de partida:</b>
+            Luverci Pereira de Souza, 2410
+        </p>
+
+        <p><b>Endereço final:</b>
+            Av. Francisco Glicério, 829
+        </p>
+
+        <p><b>Percurso:</b>
+            50 quilomêtros
+         </p>
+
+        <p><b>Valor por km: </b>
+            R$ 3,00 Serviço: 100,00
+        </p>
     </div>
 
     <div class="blog-header">
